bc264945fe0f9235f96f3ba43d4bc68945b0d027
Jean Robin <jeanrobin49@gmail.com>
2023-01-27 15:52:03 +0000

    sources and tests chapters

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,7 +1,8 @@
 select
     id as payment_id,
     orderid as order_id,
+    paymentmethod as payment_method,
     amount / 100 as amount,
     status,
     created as created_at
-from dbt-tutorial.stripe.payment
+from {{ source('stripe', 'payment') }}
